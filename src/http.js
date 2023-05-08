06305189cb6fe47422587e00925eb2815e728dce
muravevas <muravevartem@outlook.com>
2023-05-08 23:21:39 +0400

    Fixed api baseUrl

--- src/http.js
+++ src/http.js
@@ -3,7 +3,9 @@ import {userService} from "./service/UserService.js";
 import {events, eventService} from "./service/EventService.js";
 
 let api = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: (process.env.NODE_ENV === 'development')
+        ? 'http://localhost:8080'
+        : 'https://api.1304294-cu57808.tw1.ru',
     withCredentials: true,
 });
 
